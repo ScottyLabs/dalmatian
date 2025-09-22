cc3a1818d919a045c470c354a81fce15d8a90b45
Yiyoung <yiyoung.md@gmail.com>
2025-09-22 13:36:47 -0400

    feat: autocomplete feature for commands

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,9 +1,10 @@
-import { SlashCommandBuilder, ChatInputCommandInteraction, Client }  from 'discord.js';
+import { SlashCommandBuilder, ChatInputCommandInteraction, Client, AutocompleteInteraction }  from 'discord.js';
 
 //TODO: add text commands to this interface
 export interface Command {
   data: Pick<SlashCommandBuilder, 'name' | 'toJSON'>;
   execute: (interaction: ChatInputCommandInteraction) => void;
+  autocomplete?: (client: Client, interaction: AutocompleteInteraction) => void;
 }
 
 export interface Event {
