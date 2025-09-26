7e2f001992490bb7202d21753a8a994faf627642
Aromia <63314208+Arom1a@users.noreply.github.com>
2025-09-25 21:41:46 -0400

    chore: formatter and linter setup & docs (#7)
    * chore: formatter and linter setup & docs
    
    * chore: format and lint code
    
    * docs: fix typo
    
    * docs: chore
    
    * chore: fix style

--- src/commands/dining.ts
+++ src/commands/dining.ts
@@ -1,87 +1,102 @@
-import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
-import { Command } from "../types";
+import { EmbedBuilder, SlashCommandBuilder } from "discord.js";
 import { search } from "fast-fuzzy";
+import type { Command } from "../types";
 
 interface Time {
-  day: number,
-  hour: number,
-  minute: number,
+  day: number;
+  hour: number;
+  minute: number;
 }
 
 interface Location {
-  conceptID: number,
-  name: string,
-  shortDescription: string,
-  description: string,
-  url: string,
-  location: string,
+  conceptID: number;
+  name: string;
+  shortDescription: string;
+  description: string;
+  url: string;
+  location: string;
   coordinates: {
-    lat: number,
-    lng: number
-  },
-  acceptsOnlineOrders: boolean,
-  times:{
-      start: Time,
-      end: Time
-    }[]
+    lat: number;
+    lng: number;
+  };
+  acceptsOnlineOrders: boolean;
+  times: {
+    start: Time;
+    end: Time;
+  }[];
 }
 
 function getLocations(): Promise<Location[]> {
-  const request : RequestInfo = new Request("https://dining.apis.scottylabs.org/locations", {
-    method: "GET",
-  });
+  const request: RequestInfo = new Request(
+    "https://dining.apis.scottylabs.org/locations",
+    {
+      method: "GET",
+    },
+  );
 
   return fetch(request)
-    .then(res => res.json())
-    .then(data => {
+    .then((res) => res.json())
+    .then((data) => {
       return data.locations as Location[];
     });
 }
 
-function isBetween(now : Time, start : Time, end : Time) : boolean {
+function isBetween(now: Time, start: Time, end: Time): boolean {
   if (start.day === now.day || end.day === now.day) {
-    return start.hour < now.hour && now.hour < end.hour
-      || (start.hour === now.hour && start.minute <= now.minute)
-      || (end.hour === now.hour && now.minute <= end.minute);
+    return (
+      (start.hour < now.hour && now.hour < end.hour) ||
+      (start.hour === now.hour && start.minute <= now.minute) ||
+      (end.hour === now.hour && now.minute <= end.minute)
+    );
   }
   return start.day < now.day && now.day < end.day;
 }
 
-function isOpen(location: Location, time : Time) : boolean {
+function isOpen(location: Location, time: Time): boolean {
   for (const openTime of location.times)
-    if(isBetween(time, openTime.start, openTime.end))
-      return true;
+    if (isBetween(time, openTime.start, openTime.end)) return true;
   return false;
 }
 
-function formatLocation(location : Location) : EmbedBuilder {
-  if(!location) {
+function formatLocation(location: Location): EmbedBuilder {
+  if (!location) {
     return new EmbedBuilder()
       .setTitle("Dining Location Not Found")
       .setDescription("The specified dining location could not be found.");
   }
 
-  let embed = new EmbedBuilder()
+  const embed = new EmbedBuilder()
     .setTitle(location.name)
     .setDescription(location.description)
     .addFields(
       { name: "Location", value: location.location },
-      { name: "Today's Hours", value: location.times.filter(time => {
-        const now = new Date();
-        return time.start.day === now.getDay();
-      }).map(time => {
-        const startHour = time.start.hour;
-        const startMinute = time.start.minute < 10 ? `0${time.start.minute}` : time.start.minute;
+      {
+        name: "Today's Hours",
+        value:
+          location.times
+            .filter((time) => {
+              const now = new Date();
+              return time.start.day === now.getDay();
+            })
+            .map((time) => {
+              const startHour = time.start.hour;
+              const startMinute =
+                time.start.minute < 10
+                  ? `0${time.start.minute}`
+                  : time.start.minute;
 
-        const endHour = time.end.hour;
-        const endMinute = time.end.minute < 10 ? `0${time.end.minute}` : time.end.minute;
-        return `${startHour}:${startMinute} - ${endHour}:${endMinute}`;
-      }
-      ).join(", ") || "Closed" }
+              const endHour = time.end.hour;
+              const endMinute =
+                time.end.minute < 10 ? `0${time.end.minute}` : time.end.minute;
+              return `${startHour}:${startMinute} - ${endHour}:${endMinute}`;
+            })
+            .join(", ") || "Closed",
+      },
     )
-    .addFields(
-      { name: "Accepts Online Orders", value: location.acceptsOnlineOrders ? "Yes" : "No" }
-    )
+    .addFields({
+      name: "Accepts Online Orders",
+      value: location.acceptsOnlineOrders ? "Yes" : "No",
+    })
     .setURL(location.url);
 
   const url = `https://maps.googleapis.com/maps/api/staticmap?center=${location.coordinates.lat},${location.coordinates.lng}&zoom=17&size=400x200&markers=color:red%7C${location.coordinates.lat},${location.coordinates.lng}&key=${process.env.GOOGLE_MAPS_API_KEY}`;
@@ -90,11 +105,13 @@ function formatLocation(location : Location) : EmbedBuilder {
   return embed;
 }
 
-function formatLocations(locations : Location[]) : EmbedBuilder[] {
-  if(locations.length === 0) {
-    return [new EmbedBuilder()
-      .setTitle("Dining Locations")
-      .setDescription("No dining locations currently open.")];
+function formatLocations(locations: Location[]): EmbedBuilder[] {
+  if (locations.length === 0) {
+    return [
+      new EmbedBuilder()
+        .setTitle("Dining Locations")
+        .setDescription("No dining locations currently open."),
+    ];
   }
   // embed field limit is 25, new embed for every 25
   const embeds = [];
@@ -109,25 +126,34 @@ function formatLocations(locations : Location[]) : EmbedBuilder[] {
     }
 
     // TODO: refactor
-    let locationFieldBody : string = "Today's Hours: " + location.times.filter(time => {
-      const now = new Date();
-      return time.start.day === now.getDay();
-    }).map(time => {
-      const startHour = time.start.hour;
-      const startMinute = time.start.minute < 10 ? `0${time.start.minute}` : time.start.minute;
+    let locationFieldBody: string =
+      "Today's Hours: " +
+      location.times
+        .filter((time) => {
+          const now = new Date();
+          return time.start.day === now.getDay();
+        })
+        .map((time) => {
+          const startHour = time.start.hour;
+          const startMinute =
+            time.start.minute < 10
+              ? `0${time.start.minute}`
+              : time.start.minute;
 
-      const endHour = time.end.hour;
-      const endMinute = time.end.minute < 10 ? `0${time.end.minute}` : time.end.minute;
-      return `${startHour}:${startMinute} - ${endHour}:${endMinute}`;
-    }).join(", ");
+          const endHour = time.end.hour;
+          const endMinute =
+            time.end.minute < 10 ? `0${time.end.minute}` : time.end.minute;
+          return `${startHour}:${startMinute} - ${endHour}:${endMinute}`;
+        })
+        .join(", ");
     if (locationFieldBody === "Today's Hours: ") {
       locationFieldBody += "Closed";
     }
-    locationFieldBody += '\n' + location.location;
+    locationFieldBody += `\n${location.location}`;
 
     currentEmbed.addFields({
-        name: location.name,
-        value: locationFieldBody,
+      name: location.name,
+      value: locationFieldBody,
     });
   }
 
@@ -139,45 +165,62 @@ const command: Command = {
   data: new SlashCommandBuilder()
     .setName("dining")
     .setDescription("Show on-campus dining locations & hours")
-    .addSubcommand(subcommand =>
-      subcommand
-        .setName("all")
-        .setDescription("Show all dining locations"))
-    .addSubcommand(subcommand =>
+    .addSubcommand((subcommand) =>
+      subcommand.setName("all").setDescription("Show all dining locations"),
+    )
+    .addSubcommand((subcommand) =>
       subcommand
         .setName("open")
-        .setDescription("Show currently open dining locations"))
-    .addSubcommand(subcommand =>
+        .setDescription("Show currently open dining locations"),
+    )
+    .addSubcommand((subcommand) =>
       subcommand
         .setName("search")
         .setDescription("Search for a specific dining location")
-        .addStringOption(option =>
-          option.setName("query")
+        .addStringOption((option) =>
+          option
+            .setName("query")
             .setDescription("The name of the dining location to search for")
             .setRequired(true)
-            .setAutocomplete(true))),
+            .setAutocomplete(true),
+        ),
+    ),
   execute: async (interaction) => {
     const locations = await getLocations();
 
     if (interaction.options.getSubcommand() === "all") {
-      interaction.reply({ embeds: formatLocations(locations.sort((a, b) => a.name.localeCompare(b.name))) });
+      interaction.reply({
+        embeds: formatLocations(
+          locations.sort((a, b) => a.name.localeCompare(b.name)),
+        ),
+      });
       return;
     }
     if (interaction.options.getSubcommand() === "open") {
-      const rightNow : Time = {
+      const rightNow: Time = {
         day: new Date().getDay(),
         hour: new Date().getHours(),
-        minute: new Date().getMinutes()
+        minute: new Date().getMinutes(),
       };
 
-      const openLocations = locations.filter(location => isOpen(location, rightNow));
+      const openLocations = locations.filter((location) =>
+        isOpen(location, rightNow),
+      );
 
-      interaction.reply({ embeds: formatLocations(openLocations.sort((a, b) => a.name.localeCompare(b.name))) });
+      interaction.reply({
+        embeds: formatLocations(
+          openLocations.sort((a, b) => a.name.localeCompare(b.name)),
+        ),
+      });
     }
     if (interaction.options.getSubcommand() === "search") {
       const query = interaction.options.getString("query", true).toLowerCase();
-      const matchedLocations = locations.filter(location => location.name.toLowerCase().includes(query));
-      return interaction.reply({ embeds: [formatLocation(matchedLocations[0])] });
+      const matchedLocations = locations.filter((location) =>
+        location.name.toLowerCase().includes(query),
+      );
+      return interaction.reply({
+        embeds: [formatLocation(matchedLocations[0])],
+      });
     }
   },
   autocomplete: async (client, interaction) => {
@@ -185,15 +228,15 @@ const command: Command = {
 
     const locations = await getLocations();
 
-		const choices = search(focusedValue.toLowerCase(), locations, {
-			// ignoreCase is true by default
-			keySelector: (loc) => loc.name,
-		})
-			.slice(0, 25)
-			.map((loc) => ({ name: loc.name, value: loc.name }));
+    const choices = search(focusedValue.toLowerCase(), locations, {
+      // ignoreCase is true by default
+      keySelector: (loc) => loc.name,
+    })
+      .slice(0, 25)
+      .map((loc) => ({ name: loc.name, value: loc.name }));
 
     await interaction.respond(choices);
-  }
+  },
 };
 
 export default command;
--- src/handlers/event.ts
+++ src/handlers/event.ts
@@ -1,17 +1,17 @@
-import { Client } from "discord.js";
-import { readdirSync } from "fs";
-import { join } from "path";
-import { Event } from "../types";
+import { readdirSync } from "node:fs";
+import { join } from "node:path";
+import type { Client } from "discord.js";
+import type { Event } from "../types";
 
 module.exports = (client: Client) => {
-  const eventsDir = join(__dirname, '../events');
+  const eventsDir = join(__dirname, "../events");
 
   readdirSync(eventsDir).forEach((file) => {
-    if (!file.endsWith('.js')) return;
+    if (!file.endsWith(".js")) return;
     const event: Event = require(join(eventsDir, file)).default;
-    event.once ?
-      client.once(event.name, (...args) => event.execute(client, ...args)) :
-      client.on(event.name, (...args) => event.execute(client, ...args));
+    event.once
+      ? client.once(event.name, (...args) => event.execute(client, ...args))
+      : client.on(event.name, (...args) => event.execute(client, ...args));
     console.log(`Loaded event ${event.name}`);
   });
-}
+};
