5a5d1bc0c0195455c7b1b4f040dc22cc24a829c1
Gostmeaper <johnchangcao@gmail.com>
2025-12-13 18:28:58 -0500

    feat: admit incoming student context menu command

--- src/handlers/command.ts
+++ src/handlers/command.ts
@@ -3,11 +3,12 @@ import { join } from "node:path";
 
 import {
     type Client,
+    type ContextMenuCommandBuilder,
     REST,
     Routes,
     type SlashCommandBuilder,
 } from "discord.js";
-import type { Command } from "../types.d.ts";
+import type { Command, ContextCommand } from "../types.d.ts";
 
 export default (client: Client) => {
     const commands: Pick<SlashCommandBuilder, "name" | "toJSON">[] = [];
@@ -39,6 +40,53 @@ export default (client: Client) => {
                         : "unknown";
                     console.log(
                         `Successfully reloaded ${commandCount} application (/) commands.`,
+                    );
+                });
+        } catch (error: unknown) {
+            console.error(
+                error instanceof Error ? error.message : String(error),
+            );
+        }
+    })().catch((error: unknown) => {
+        console.error(error instanceof Error ? error.message : String(error));
+    });
+
+    const contextCommands: Pick<
+        ContextMenuCommandBuilder,
+        "name" | "toJSON"
+    >[] = [];
+
+    const contextDir = join(__dirname, "../contextCommands");
+    readdirSync(contextDir).forEach((file) => {
+        if (!file.endsWith(".ts")) return;
+
+        const command: ContextCommand = require(join(contextDir, file)).default;
+        contextCommands.push(command.data);
+        client.contextCommands.set(command.data.name, command);
+    });
+
+    (async () => {
+        try {
+            console.log("Started refreshing global context menu commands...");
+
+            await rest
+                .put(
+                    Routes.applicationGuildCommands(
+                        process.env.DISCORD_CLIENT_ID,
+                        "1449512254541139980",
+                    ),
+                    {
+                        body: contextCommands.map((command) =>
+                            command.toJSON(),
+                        ),
+                    },
+                )
+                .then((data: unknown) => {
+                    const commandCount = Array.isArray(data)
+                        ? data.length
+                        : "unknown";
+                    console.log(
+                        `Successfully reloaded ${commandCount} global context menu commands.`,
                     );
                 });
         } catch (error: unknown) {
