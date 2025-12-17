387a83aa1466e97b5fbe79cc3b4f5e76ecebc657
Gostmeaper <johnchangcao@gmail.com>
2025-12-17 04:20:08 -0500

    purge all

---
+++ src/commands/purge.ts
@@ -0,0 +1,107 @@
+import {
+    BaseGuildTextChannel,
+    Message,
+    MessageFlags,
+    PermissionFlagsBits,
+    SlashCommandBuilder,
+} from "discord.js";
+import type { SlashCommand } from "../types.d.ts";
+
+// Command for purging messages in a channel with bulk delete
+const command: SlashCommand = {
+    data: new SlashCommandBuilder()
+        .setName("purge")
+        .setDescription("removes messages in channel")
+        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName("all")
+                .setDescription(
+                    "removes all messages newer than 2 weeks up to 100",
+                )
+                .addIntegerOption((option) =>
+                    option
+                        .setName("count")
+                        .setDescription("Number of messages to delete")
+                        .setRequired(true),
+                ),
+        )
+        .addSubcommand((subcommand) =>
+            subcommand
+                .setName("user")
+                .setDescription("removes all messages from a user")
+                .addUserOption((option) =>
+                    option
+                        .setName("user")
+                        .setDescription("User to remove messages from")
+                        .setRequired(true),
+                )
+                .addIntegerOption((option) =>
+                    option
+                        .setName("count")
+                        .setDescription("Number of messages to delete")
+                        .setRequired(true),
+                ),
+        ),
+
+    async execute(interaction) {
+        const channel = interaction.channel;
+
+        if (!channel || !(channel instanceof BaseGuildTextChannel)) {
+            return interaction.reply({
+                content: "Not valid text channel",
+                flags: MessageFlags.Ephemeral,
+            });
+        }
+
+        const count = Number(interaction.options.getInteger("count"));
+        if (count < 1) {
+            return interaction.editReply(`invalid count`);
+        }
+
+        await interaction.deferReply({ flags: MessageFlags.Ephemeral });
+
+        // given num of messages deleted bulk delete messages before switching to one by one
+        if (interaction.options.getSubcommand() === "all") {
+            let remaining = count;
+            let deletedMessages: Message[] = [];
+
+            while (remaining > 0) {
+                const numDelete = Math.min(remaining, 100);
+                const messages = await channel.bulkDelete(numDelete, true);
+
+                if (messages.size == 0) {
+                    break;
+                }
+
+                deletedMessages.push(
+                    ...Array.from(messages.values()).filter(
+                        (m): m is Message => !!m && !m.partial,
+                    ),
+                );
+                remaining -= messages.size;
+            }
+
+            while (remaining > 0) {
+                const numDelete = Math.min(remaining, 100);
+                const messages = await channel.messages.fetch({
+                    limit: numDelete,
+                });
+
+                for (const message of messages.values()) await message.delete();
+
+                deletedMessages.push(...messages.values());
+                remaining -= messages.size;
+            }
+
+            return interaction.editReply(
+                `Purged ${deletedMessages.length} messages`,
+            );
+        }
+        if (interaction.options.getSubcommand() === "user") {
+            return interaction.editReply("User purge not implemented yet");
+        }
+    },
+};
+
+export default command;
