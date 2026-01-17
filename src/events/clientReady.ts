36e3890079edaa2401d0d9e747d3e8cef0594cdf
Yiyoung Liu <yiyoung.md@gmail.com>
2026-01-17 17:05:09 -0500

    chore: delete caching on bot start
    it runs into logistical issues

--- src/events/clientReady.ts
+++ src/events/clientReady.ts
@@ -1,46 +1,10 @@
-import {
-    Client,
-    Collection,
-    Events,
-    Guild,
-    GuildMember,
-    Role,
-} from "discord.js";
+import { Client, Events } from "discord.js";
 import type { Event } from "../types.d.ts";
-
-const cacheRoles = (guild: Guild) =>
-    guild.roles.fetch().then((roles: Collection<string, Role>) => roles.size);
-
-const cacheMembers = (guild: Guild) =>
-    guild.members
-        .fetch()
-        .then((members: Collection<string, GuildMember>) => members.size);
-
-const cacheGuildItems = (guild: Guild) =>
-    Promise.all([cacheRoles(guild), cacheMembers(guild)]).then(
-        ([roleCount, memberCount]) => {
-            console.log(
-                `Cached ${roleCount} roles and ${memberCount} members for guild: ${guild.name}`,
-            );
-        },
-    );
-
-const cacheGuilds = (client: Client) =>
-    client.guilds
-        .fetch()
-        .then((guilds) =>
-            Promise.all(
-                [...guilds.values()].map((g) =>
-                    g.fetch().then(cacheGuildItems),
-                ),
-            ),
-        );
 
 const event: Event<Events.ClientReady> = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
-        cacheGuilds(client);
         console.log(`Logged in as ${client.user.tag}`);
     },
 };
