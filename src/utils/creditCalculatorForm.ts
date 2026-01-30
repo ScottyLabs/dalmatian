c188641e6a46aa888218b054397a4487a0b350d7
Gostmeaper <johnchangcao@gmail.com>
2026-01-30 18:11:55 -0500

    fix: not ephemeral

--- src/utils/creditCalculatorForm.ts
+++ src/utils/creditCalculatorForm.ts
@@ -64,7 +64,7 @@ export class SetupForm {
     }
 
     async start(): Promise<void> {
-        await this.interaction.deferReply({ flags: MessageFlags.Ephemeral });
+        await this.interaction.deferReply();
         await this.showForm();
     }
 
