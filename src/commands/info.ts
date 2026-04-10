a9891c0223e4e3a8c94c61747fe6981579461bef
Sushrut <xboxbedrock@gmail.com>
2026-04-10 16:26:26 -0700

    feat(info): update formatting 2.0

--- src/commands/info.ts
+++ src/commands/info.ts
@@ -1,4 +1,10 @@
-import { ActionRowBuilder, ButtonBuilder, ButtonStyle, EmbedBuilder, SlashCommandBuilder } from "discord.js";
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    EmbedBuilder,
+    SlashCommandBuilder,
+} from "discord.js";
 import type { SlashCommand } from "../types.js";
 import ms from "ms";
 import { DEFAULT_EMBED_COLOR } from "../constants.ts";
@@ -8,15 +14,27 @@ const command: SlashCommand = {
         .setName("info")
         .setDescription("Get information about the bot"),
     async execute(interaction) {
-
-        const commit = (await (await fetch("https://api.github.com/repos/scottylabs/dalmatian/commits")).json()) as {
-            author: { login: string, html_url: string };
-            commit: {committer: {date: string}}
+        const commit = (await (
+            await fetch(
+                "https://api.github.com/repos/scottylabs/dalmatian/commits",
+            )
+        ).json()) as {
+            author: { login: string; html_url: string };
+            commit: { committer: { date: string } };
         }[];
 
-        const latestContributors = commit.filter((c) => new Date(c.commit.committer.date) > new Date(Date.now() - 7 * 24 * 60 * 60 * 1000)) //last 7 days
-            .map((c) => ({username: c.author.login, url: c.author.html_url}))
-            .filter((value, index, self) => index === self.findIndex((t) => t.username === value.username))
+        const latestContributors = commit
+            .filter(
+                (c) =>
+                    new Date(c.commit.committer.date) >
+                    new Date(Date.now() - 7 * 24 * 60 * 60 * 1000),
+            ) //last 7 days
+            .map((c) => ({ username: c.author.login, url: c.author.html_url }))
+            .filter(
+                (value, index, self) =>
+                    index ===
+                    self.findIndex((t) => t.username === value.username),
+            )
             .slice(0, 5); //top 5 max
 
         const embed = new EmbedBuilder()
@@ -43,13 +61,17 @@ const command: SlashCommand = {
                 },
                 {
                     name: "Latest Contributors",
-                    value: latestContributors.length > 0
-                        ? latestContributors.map(c => `[${c.username}](${c.url})`).join(", ")
-                        : "No contributors in the last 7 days",
+                    value:
+                        (latestContributors.length > 0
+                            ? latestContributors
+                                  .map((c) => `[${c.username}](${c.url})`)
+                                  .join(", ")
+                            : "No contributors in the last 7 days") +
+                        (process.env["COMMIT_HASH"]
+                            ? `\n\n-# commit [\`${process.env["COMMIT_HASH"].slice(0, 7)}\`](https://github.com/ScottyLabs/dalmatian/commit/${process.env["COMMIT_HASH"]})`
+                            : ""),
                 },
-            )
-    
-        if (process.env['COMMIT_HASH']) embed.setFooter({text: `Current Version: [${process.env['COMMIT_HASH'].slice(0, 7)}](https://github.com/ScottyLabs/dalmatian/commit/${process.env['COMMIT_HASH']})`});
+            );
 
         const components = new ActionRowBuilder<ButtonBuilder>().addComponents(
             new ButtonBuilder()
@@ -59,12 +81,12 @@ const command: SlashCommand = {
             new ButtonBuilder()
                 .setLabel("Bug Report")
                 .setStyle(ButtonStyle.Link)
-                .setURL("https://github.com/ScottyLabs/dalmatian/issues/new")
+                .setURL("https://github.com/ScottyLabs/dalmatian/issues/new"),
         );
 
         await interaction.reply({
             embeds: [embed],
-            components: [components]
+            components: [components],
         });
     },
 };
