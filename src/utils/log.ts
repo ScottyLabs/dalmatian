72771220e0dd0167d93ac038de6e1392e0d2c771
Sushrut <xboxbedrock@gmail.com>
2026-05-21 09:29:01 -0700

    fix: format

--- src/utils/log.ts
+++ src/utils/log.ts
@@ -24,7 +24,7 @@ export async function configureLogger() {
                 category: ["logtape", "meta"],
                 lowestLevel: "warning",
                 sinks: ["console"],
-            }
+            },
         ],
     });
 }
